fe9e43d1e74a31794ec50a70cd07f820eb99b4ab
Camilo138 <camilovalle138@gmail.com>
2024-09-19 01:04:47 -0500

    Consultas SQL (CodeMusic)

--- SQL/scripts/Consultas CODEMUSIC.sql
+++ SQL/scripts/Consultas CODEMUSIC.sql
@@ -1,31 +1,40 @@
--- 0. Imprime las 5 canciones con mas duracion
+-- 1. Imprime las 5 canciones con mas duracion
 
-SELECT c.titulo_cancion, ar.nombre_artista, c.duracion
-FROM canciones c
-JOIN artistas ar ON c.id_artista = ar.id_artista
-ORDER BY c.duracion DESC
-LIMIT 5;
+SELECT sub.titulo_cancion, sub.nombre_artista, sub.duracion
+FROM (
+    SELECT c.titulo_cancion, ar.nombre_artista, c.duracion
+    FROM canciones c
+    JOIN artistas ar ON c.id_artista = ar.id_artista
+    ORDER BY c.duracion DESC
+    LIMIT 5
+) AS sub
+ORDER BY sub.titulo_cancion;
 
--- 1. Imprime canciones de un artista especifico e imprime la duracion de la cancion y el album al que pertenece la cancion
 
-SELECT c.titulo_cancion, c.duracion, a.titulo_album 
-FROM canciones c
-JOIN albumes a ON c.id_album = a.id_album
-JOIN artistas ar ON c.id_artista = ar.id_artista
-WHERE ar.nombre_artista = 'Shakira';
+-- 2. Imprime canciones de un artista especifico e imprime la duracion de la cancion y el album al que pertenece la cancion
 
--- 2. Imprime el nombre del artista y el album del artista
+SELECT *
+FROM (
+    SELECT c.titulo_cancion, c.duracion, a.titulo_album 
+    FROM canciones c
+    JOIN albumes a ON c.id_album = a.id_album
+    JOIN artistas ar ON c.id_artista = ar.id_artista
+    WHERE ar.nombre_artista = 'Shakira'
+) AS sub
+WHERE sub.duracion > 180;
 
-SELECT ar.nombre_artista, a.titulo_album
-FROM artistas ar
-LEFT JOIN albumes a ON ar.id_artista = a.id_artista;
 
--- 3. Imprime el album y el genero musical del album
+-- 3. Imprimir el álbum y el género dentro de una consulta que excluye géneros 'Pop'
 
-SELECT a.titulo_album, g.nombre_genero
-FROM albumes a
-RIGHT JOIN artistas ar ON a.id_artista = ar.id_artista
-RIGHT JOIN generos g ON ar.id_genero = g.id_genero;
+SELECT sub.titulo_album, sub.nombre_genero
+FROM (
+    SELECT a.titulo_album, g.nombre_genero
+    FROM albumes a
+    JOIN artistas ar ON a.id_artista = ar.id_artista
+    JOIN generos g ON ar.id_genero = g.id_genero
+) AS sub
+WHERE sub.nombre_genero != 'Pop';
+
 
 -- 4. Mostrar todas las canciones y los álbumes a los que pertenecen, pero también todas las canciones que no están asociadas a ningún álbum
 
@@ -36,9 +45,5 @@ UNION
 SELECT c.titulo_cancion, a.titulo_album
 FROM canciones c
 RIGHT JOIN albumes a ON c.id_album = a.id_album;
-
-
-
-
 
 
